c867d1fd7004dc869917beb8182a0aa3b1833777
Piotr <pituzz.01@gmail.com>
2020-08-16 21:06:37 +0200

    sdcs

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import Layout from '../../components/Layout'
+
 import BlogRoll from '../../components/BlogRoll'
+import Layout from "../../components/layout";
 
 export default class BlogIndexPage extends React.Component {
   render() {
