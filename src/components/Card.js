2ad52135f1f1f64e891d607971dd631fcb1a4701
Piotr <pituzz.01@gmail.com>
2020-08-16 22:45:15 +0200

    sdcs

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-
-export const Card = styled.div`
-  border-radius: 23px;
-  margin: 75px 38px;
-  background: white;
-  padding: 50px 30px;
-  box-shadow: 0px -1px 64px #0000000f;
-`
--- src/components/FeaturedButton/FeaturedButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { FeaturedButtonContainer } from "./FeaturedButton.styles"
-
-const FeaturedButton = ({ children, to }) => {
-  return <FeaturedButtonContainer to={to}>{children}</FeaturedButtonContainer>
-}
-
-export default FeaturedButton
